4c7feae2ebc7c20dd1f71a50533fc1f70943d5a3
Sourav Ganguly <isouravganguly@gmail.com>
2022-04-25 07:51:32 -0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,7 +35,22 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 // ========================== ACORDIAN SKILLS ===========================
 
+// ====================== PORTFOLIO Swiper ==============================
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
 
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
 
 // ================ QUALIFICATION TABS ==========================
 
